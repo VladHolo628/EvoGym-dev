52744de0d2e60bba627db3d84aa4e200a6a2ab2e
Vlad Holo <holovlad@yandex.ru>
2023-07-23 22:59:48 +1000

    Fixed Action Button

--- src/shared/ui/ActionButton/ui.tsx
+++ src/shared/ui/ActionButton/ui.tsx
@@ -10,7 +10,7 @@ export const ActionButton = ({
     <AnchorLink
       onClick={() => setSelectedPage(SelectedPage.ContactUs)}
       className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
-      href={`${SelectedPage.ContactUs}`}
+      href={`#${SelectedPage.ContactUs}`}
     >
       {children}
     </AnchorLink>
