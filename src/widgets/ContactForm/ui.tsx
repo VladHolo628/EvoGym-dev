522e59e1fee1cefc00ba769b71db3960b4fe7f13
Vlad Holo <holovlad@yandex.ru>
2023-07-26 20:14:05 +1000

    Added success popup for Form

--- src/widgets/ContactForm/ui.tsx
+++ src/widgets/ContactForm/ui.tsx
@@ -1,23 +1,39 @@
-import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 export const ContactForm = () => {
+  const [popupIsShown, setPopupIsShown] = useState<boolean>(false);
+
   const inputStyles = `mt-5 mb-2 w-full rounded-lg bg-primary-300
     px-5 py-3 placeholder-white relative`;
 
   const emailPattern =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+  const formSubmitHander = (data: FieldValues) => {
+    console.log(data);
+    setPopupIsShown(true);
+    setTimeout(() => {
+      setPopupIsShown(false);
+    }, 2000);
+  };
+
   const {
     register,
     handleSubmit,
+    reset,
+    formState,
     formState: { errors },
   } = useForm();
 
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset();
+    }
+  }, [formState, reset]);
+
   return (
-    <form
-      onSubmit={handleSubmit((data) => console.log(data))}
-      className="w-full"
-    >
+    <form onSubmit={handleSubmit(formSubmitHander)} className="w-full relative">
       <input
         className={inputStyles}
         type="text"
@@ -46,7 +62,7 @@ export const ContactForm = () => {
       {errors.email && (
         <p className="mt-1 text-primary-500">
           {errors.email.type === "required" && "This Field Is Required"}
-          {errors.email.type === "maxLength" && "Invalid E-Mail Address"}
+          {errors.email.type === "pattern" && "Invalid E-Mail Address"}
         </p>
       )}
 
@@ -74,6 +90,11 @@ export const ContactForm = () => {
           SUBMIT
         </button>
       </div>
+      {popupIsShown && (
+        <div className="w-2/5 h-2/5  absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-primary-100 rounded-md shadow-2xl flex justify-center items-center uppercase">
+          <p className="uppercase font-bold">Succsessfully Submitted !</p>
+        </div>
+      )}
     </form>
   );
 };
